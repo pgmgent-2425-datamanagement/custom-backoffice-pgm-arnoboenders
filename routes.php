f894f9463f1ca2144c45abfe808ff0d2d7e7d66b
pgm-arnoboenders <arno.boenders@student.arteveldehs.be>
2024-10-10 14:51:25 +0200

    added routes for user->id and projects

--- routes.php
+++ routes.php
@@ -3,4 +3,9 @@
 //$router->get('/', function() { echo 'Dit is de index vanuit de route'; });
 $router->setNamespace('\App\Controllers');
 $router->get('/', 'HomeController@index');
+// users
 $router->get('/users', 'UserController@index');
+$router->get('/users/(\d+)', 'UserController@show');
+// projects
+$router->get('/projects', 'ProjectController@index');
+$router->get('/project/(\d+)', 'ProjectController@show');
