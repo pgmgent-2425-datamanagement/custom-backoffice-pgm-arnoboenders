830dcd3ad53710b7007d5a76a2bbfb9fbbe82dd5
pgm-arnoboenders <arno.boenders@student.arteveldehs.be>
2024-10-10 14:51:40 +0200

    udpate route projects->id

--- routes.php
+++ routes.php
@@ -8,4 +8,4 @@ $router->get('/users', 'UserController@index');
 $router->get('/users/(\d+)', 'UserController@show');
 // projects
 $router->get('/projects', 'ProjectController@index');
-$router->get('/project/(\d+)', 'ProjectController@show');
+$router->get('/projects/(\d+)', 'ProjectController@show');
