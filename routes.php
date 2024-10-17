72f97c4461dc2c14212d14c3f0ce3374ba1b0b02
pgm-arnoboenders <arno.boenders@student.arteveldehs.be>
2024-10-17 12:59:01 +0200

    added routes

--- routes.php
+++ routes.php
@@ -3,11 +3,23 @@
 //$router->get('/', function() { echo 'Dit is de index vanuit de route'; });
 $router->setNamespace('\App\Controllers');
 $router->get('/', 'HomeController@index');
+
 // users
 $router->get('/users', 'UserController@index');
 $router->get('/users/(\d+)', 'UserController@show');
+
 // projects
 $router->get('/projects', 'ProjectController@index');
 $router->get('/projects/(\d+)', 'ProjectController@show');
+
 $router->get('/projects/create', 'ProjectController@create');
 $router->post('/projects/create', 'ProjectController@create');
+
+$router->get('/projects/edit/(\d+)', 'ProjectController@edit');
+$router->post('/projects/edit/(\d+)', 'ProjectController@edit');
+
+$router->get('/projects/delete/(\d+)', 'ProjectController@delete');
+
+// tasks
+$router->get('/tasks', 'TaskController@index');
+$router->get('/tasks/(\d+)', 'TaskController@show');
