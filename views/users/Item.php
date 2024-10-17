75f6465e102c9be21978e5857a0df5c572cef7bf
pgm-arnoboenders <arno.boenders@student.arteveldehs.be>
2024-10-17 13:24:32 +0200

    udpate styling for user item

--- views/users/Item.php
+++ views/users/Item.php
@@ -1,7 +1,7 @@
 <div class="user-item">
-    <a href="/users/<?= $user->id ?>">
-        <p><strong>ID:</strong> <?= $user->id ?></p>
-        <p><strong>Name:</strong> <?= $user->first_name . ' ' . $user->last_name ?></p>
-        <p><strong>Email:</strong> <?= $user->email ?></p>
-    </a>
+    <img src="/path/to/user/avatar.jpg" alt="<?= htmlspecialchars($user->first_name . ' ' . $user->last_name) ?>" width="50" height="50">
+    <div class="user-details">
+        <div class="user-name"><?= htmlspecialchars($user->first_name . ' ' . $user->last_name) ?></div>
+        <div class="user-email"><?= htmlspecialchars($user->email) ?></div>
+    </div>
 </div>
