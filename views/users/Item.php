db5194796db88c08e0e4e7f8217199849ec87f52
pgm-arnoboenders <arno.boenders@student.arteveldehs.be>
2024-10-17 13:50:03 +0200

    added edit and delete buttons with styling

--- views/users/Item.php
+++ views/users/Item.php
@@ -4,4 +4,8 @@
         <div class="user-name"><?= htmlspecialchars($user->first_name . ' ' . $user->last_name) ?></div>
         <div class="user-email"><?= htmlspecialchars($user->email) ?></div>
     </div>
+    <div class="user-actions">
+        <a href="/edit-user.php?id=<?= urlencode($user->id) ?>" class="btn btn-edit">Edit</a>
+        <a href="/delete-user.php?id=<?= urlencode($user->id) ?>" class="btn btn-remove" onclick="return confirm('Are you sure you want to delete this user?');">Remove</a>
+    </div>
 </div>
