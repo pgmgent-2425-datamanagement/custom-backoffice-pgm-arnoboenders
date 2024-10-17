0029c1841c916ab5bc0b8bf31fe08c3208b284fc
pgm-arnoboenders <arno.boenders@student.arteveldehs.be>
2024-10-17 12:57:24 +0200

    update project model

--- Models/Project.php
+++ Models/Project.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 class Project extends BaseModel
@@ -7,23 +8,59 @@ class Project extends BaseModel
     protected $pk = 'id';
     protected $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function all() {
+    protected function all()
+    {
         return parent::all();
     }
 
-    public function find(int $id) {
+    protected function find(int $id)
+    {
         return parent::find($id);
     }
 
-    public function create(array $data) {
-        return parent::create($data);
+    public function create(array $data)
+    {
+        $sql = 'INSERT INTO `' . $this->table . '` (`name`, `description`, `start_date`, `end_date`, `manager_id`, `status_id`) VALUES (:name, :description, :start_date, :end_date, :manager_id, :status_id)';
+        $pdo_statement = $this->db->prepare($sql);
+        $pdo_statement->execute([
+            ':name' => $data['name'],
+            ':description' => $data['description'],
+            ':start_date' => date('Y-m-d H:i:s'),
+            ':end_date' => $data['end_date'],
+            ':manager_id' => $data['manager'],
+            ':status_id' => $data['status']
+        ]);
     }
 
-    public function update(int $id, array $data) {
+    public function update(int $id, array $data)
+    {
         return parent::update($id, $data);
+    }
+    public function save()
+    {
+        $sql = 'UPDATE `' . $this->table . '` SET `name` = :name, `description` = :description, `start_date` = :start_date, `end_date` = :end_date, `manager_id` = :manager_id, `status_id` = :status_id WHERE `' . $this->pk . '` = :id';
+        $pdo_statement = $this->db->prepare($sql);
+        $pdo_statement->execute([
+            ':name' => $this->name,
+            ':description' => $this->description,
+            ':start_date' => $this->start_date,
+            ':end_date' => $this->end_date,
+            ':manager_id' => $this->manager_id,
+            ':status_id' => $this->status_id,
+            ':id' => $this->id
+        ]);
+    }
+    public function delete()
+    {
+        $sql = 'DELETE FROM `' . $this->table . '` WHERE `' . $this->pk . '` = :id';
+        $pdo_statement = $this->db->prepare($sql);
+        $pdo_statement->execute([
+            ':id' => $this->id
+        ]);
     }
 }
