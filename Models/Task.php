95872ef4810c6e3bb1237c19a6692f98e456b1c0
pgm-arnoboenders <arno.boenders@student.arteveldehs.be>
2024-10-17 12:57:58 +0200

    added tasks and comments model

---
+++ Models/Task.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Models;
+
+class Task extends BaseModel
+{
+    protected $table = 'tasks';
+    protected $pk = 'id';
+    protected $db;
+    public function __construct() {
+        parent::__construct();
+    }
+    public function all() {
+        return parent::all();
+    }
+    public function find(int $id) {
+        return parent::find($id);
+    }
+    public function create(array $data) {
+        return parent::create($data);
+    }
+    public function update(int $id, array $data) {
+        return parent::update($id, $data);
+    }
+}
